4abd7df6a147db8068d22ba230960a0798a028fa
Michal Andrzejczak <236489@edu.p.lodz.pl>
2022-03-14 18:16:51 +0100

    Etap 0

---
+++ BankTest/ExampleProgramTest.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class ExampleProgramTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            double money1 = 12;
+            double money2 = 50;
+
+            BankAccount one = new BankAccount("Jon Smith", money1);
+            BankAccount two = new BankAccount("Joanna Dark", money2);
+            Assert.AreNotEqual(one.getMoney(), two.getMoney()); 
+            Assert.AreEqual(money1, one.getMoney());
+            Assert.AreEqual(money2, two.getMoney());
+            
+        }
+
+        [TestMethod]
+        public void MoneyOutTest()
+        {
+            double money1 = 12;
+
+            BankAccount one = new BankAccount("Jon Smith", money1);
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => one.MoneyOut(200));
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => one.MoneyOut(0));
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => one.MoneyOut(-5));
+            Assert.AreEqual(money1, one.getMoney());
+
+            one.MoneyOut(5);
+            Assert.AreEqual(7, one.getMoney());
+        }
+
+        [TestMethod]
+        public void MoneyInTest()
+        {
+            double money1 = 12;
+
+            BankAccount one = new BankAccount("Jon Smith", money1);
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => one.MoneyIn(0));
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => one.MoneyIn(-5));
+            Assert.AreEqual(money1, one.getMoney());
+
+            one.MoneyIn(5);
+            Assert.AreEqual(17, one.getMoney());
+        }
+    }
+}
---
+++ ProWsp/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        private readonly string clientName;
+        private double money;
+      
+        public BankAccount(string cN, double m)
+        {
+            clientName  = cN;
+            money = m;
+        }
+
+
+        public void MoneyOut(double amount)
+        {
+            if(amount > money | amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{amount} is invalid argument");
+            }
+
+            money -= amount;
+        }
+        public void MoneyIn(double amount)
+        {
+            if(amount <= 0) {
+                throw new ArgumentOutOfRangeException($"{amount} is invalid argument");
+            }
+            money += amount;
+        }
+
+
+        public double getMoney()
+        {
+            return money;
+        }
+        public string getClientName()
+        {
+            return clientName;
+        }
+
+        public static void Main()
+        { 
+            BankAccount one = new BankAccount("Jon Smith", 5000);
+            Console.WriteLine($"Name of the bank client is {one.getClientName()}, money is {one.getMoney()}.");
+        }
+    }
+}
