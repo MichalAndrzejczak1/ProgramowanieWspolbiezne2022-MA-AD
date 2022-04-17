5d8a8dddc18d100ee45f17d3ea059338bfa5a628
Michal Andrzejczak <236489@edu.p.lodz.pl>
2022-04-17 22:38:42 +0200

    Initial logic and data code.

---
+++ Logic/Logic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Data;
+
+namespace BusinessLogic
+{
+    public class Logic
+    {
+        private Board _board;
+
+        public void AddBall(Ball ball)
+        {
+            _board.Balls.Add(ball);
+        }
+    }
+}
